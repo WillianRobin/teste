6678cca63d39e02af568fab49876384162da0638
Willian <willianmacuchapi8@gmail.com>
2024-07-01 11:54:31 -0300

    salvando

---
+++ teste_7/produto.js
@@ -0,0 +1,11 @@
+let numero_produto = document.getElementsByClassName('numero-produto')
+let menos = document.getElementById('menos')
+let mais = document.getElementById('mais')
+let numero1 = 1
+
+numero_produto.addEventListener()
+
+
+
+aaaaaaaa--
+aaaaaaaa++
